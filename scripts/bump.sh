9f8809b89946e0c3c6df88040c635e37b81017f6
yuler <is.yuler@gmail.com>
2025-06-20 01:18:12 +0800

    Release v0.1.2

---
+++ scripts/bump.sh
@@ -0,0 +1,26 @@
+version=$1
+
+# Validate version argument
+if [[ ! "$version" =~ ^(patch|minor|major)$ ]]; then
+  echo "Error: version must be one of: patch, minor, major"
+  echo "Usage: ./scripts/bump.sh <patch|minor|major>"
+  exit 1
+fi
+
+# Update package.json version, without tag
+pnpm version $version --no-git-tag-version
+package_version=$(cat package.json | jq -r '.version')
+
+# Update src-tauri version
+cd src-tauri
+# Update Cargo.toml version
+sed -i '' "s/^version = \".*\"/version = \"$package_version\"/g" Cargo.toml
+# Update Cargo.lock version
+cargo update typo
+
+git add -A
+git commit --amend --message "Release v$package_version"
+
+echo $package_version
+git tag v$package_version
+git push origin $package_version
