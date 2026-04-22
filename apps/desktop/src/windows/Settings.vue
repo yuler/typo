a435c29039837694c5184e9729acbc8ffc5b473a
yuler <is.yuler@gmail.com>
2026-04-23 00:32:22 +0800

    fix(desktop): use MacosLauncher::AppleScript for Login Items support and add logging

--- apps/desktop/src/windows/Settings.vue
+++ apps/desktop/src/windows/Settings.vue
@@ -236,11 +236,19 @@ onUnmounted(() => {
 })
 
 watch(() => form.value.autostart, async (value) => {
-  if (value) {
-    await enable()
+  console.log('autostart change:', value)
+  try {
+    if (value) {
+      await enable()
+      console.log('autostart enabled')
+    }
+    else {
+      await disable()
+      console.log('autostart disabled')
+    }
   }
-  else {
-    await disable()
+  catch (error) {
+    console.error('autostart error:', error)
   }
   await store.set('autostart', value)
   await store.save()
