c121b6c22bbf8424b8ca3ea2e35cfbc7ffb863fb
Yu Le <is.yuler@gmail.com>
2026-04-26 14:40:48 +0800

    ✨ [core]: Add Fizzy-style SQLite UUID defaults for new tables (#44)

---
+++ core/lib/rails_ext/active_record_uuid_type.rb
@@ -0,0 +1,50 @@
+# Custom UUID attribute type for SQLite binary storage with base36 string representation
+#  refs: https://github.com/basecamp/fizzy/blob/e6c5cd51a0b2eb3f9eab3bb39da269a90e52cd62/lib/rails_ext/active_record_uuid_type.rb
+module ActiveRecord
+  module Type
+    class Uuid < Binary
+      BASE36_LENGTH = 25 # 36^25 > 2^128
+
+      class << self
+        def generate
+          uuid = SecureRandom.uuid_v7
+          hex = uuid.delete("-")
+          hex_to_base36(hex)
+        end
+
+        def hex_to_base36(hex)
+          hex.to_i(16).to_s(36).rjust(BASE36_LENGTH, "0")
+        end
+
+        def base36_to_hex(base36)
+          base36.to_s.to_i(36).to_s(16).rjust(32, "0")
+        end
+      end
+
+      def serialize(value)
+        return super if value.is_a?(ActiveModel::Type::Binary::Data)
+        return unless value
+
+        hex = Uuid.base36_to_hex(value)
+        super([ hex ].pack("H*"))
+      end
+
+      def deserialize(value)
+        return unless value
+
+        hex = value.to_s.unpack1("H*")
+        Uuid.hex_to_base36(hex)
+      end
+
+      def cast(value)
+        if value.is_a?(String) && (value.length == 36 || value.length == 32) && value.match?(/\A[0-9a-f]{8}-?([0-9a-f]{4}-?){3}[0-9a-f]{12}\z/i)
+          Uuid.hex_to_base36(value.delete("-"))
+        else
+          super
+        end
+      end
+    end
+  end
+end
+
+ActiveRecord::Type.register(:uuid, ActiveRecord::Type::Uuid, adapter: :sqlite3)
---
+++ core/test/lib/rails_ext/active_record_uuid_type_test.rb
@@ -0,0 +1,37 @@
+# core/test/lib/rails_ext/active_record_uuid_type_test.rb
+require "test_helper"
+
+class ActiveRecordUuidTypeTest < ActiveSupport::TestCase
+  test "uuid type is registered for sqlite3 adapter" do
+    type = ActiveRecord::Type.lookup(:uuid, adapter: :sqlite3)
+
+    assert_equal "ActiveRecord::Type::Uuid", type.class.name
+  end
+
+  test "generate returns 25-char base36 token" do
+    value = ActiveRecord::Type::Uuid.generate
+
+    assert_equal 25, value.length
+    assert_match(/\A[0-9a-z]+\z/, value)
+  end
+
+  test "serialize and deserialize round trip" do
+    type = ActiveRecord::Type::Uuid.new
+    original = ActiveRecord::Type::Uuid.generate
+
+    binary = type.serialize(original)
+    round_trip = type.deserialize(binary)
+
+    assert_equal original, round_trip
+  end
+
+  test "serialize handles Binary::Data objects" do
+    type = ActiveRecord::Type::Uuid.new
+    data = ActiveModel::Type::Binary::Data.new("binary_blob")
+
+    serialized = type.serialize(data)
+
+    assert_instance_of ActiveModel::Type::Binary::Data, serialized
+    assert_equal "binary_blob", serialized.to_s
+  end
+end
