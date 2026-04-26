8b8a00aacd44bbc950a23b34e1d37a1510c6ff6c
yuler <is.yuler@gmail.com>
2026-04-26 16:18:56 +0800

    feat: implement magic link generation

--- core/app/models/magic_link.rb
+++ core/app/models/magic_link.rb
@@ -1,6 +1,16 @@
 class MagicLink < ApplicationRecord
   belongs_to :identity
   
-  validates :code, presence: true, uniqueness: true
-  validates :expires_at, presence: true
+  before_validation(on: :create) do
+    self.code ||= SecureRandom.alphanumeric(6).upcase
+    self.expires_at ||= 15.minutes.from_now
+  end
+
+  def expired?
+    expires_at < Time.current
+  end
+
+  def used?
+    used_at.present?
+  end
 end
