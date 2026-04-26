cb182293b16b5b7cf8689592e8ef57adcf149ce1
yuler <is.yuler@gmail.com>
2026-04-26 16:20:31 +0800

    fix: explicitly require account slug extractor middleware

--- core/config/application.rb
+++ core/config/application.rb
@@ -1,6 +1,7 @@
 require_relative "boot"
 
 require "rails/all"
+require_relative "../lib/account_slug/extractor"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
