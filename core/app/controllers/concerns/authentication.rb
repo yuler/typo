cda180f36406c7b4a437e6bba0f9ac7df259bc50
yuler <is.yuler@gmail.com>
2026-05-06 13:44:10 +0800

    🐛 [core]: Fix logout functionality and rename helper module
    - Rename LoginHelper to UrlHelper for clarity
    - Fix logout_url to point to correct session path
    - Implement destroy action in SessionsController
    - Update dashboard to use logout_url helper

--- core/app/controllers/concerns/authentication.rb
+++ core/app/controllers/concerns/authentication.rb
@@ -9,7 +9,7 @@ module Authentication
 
     etag { Current.identity.id if authenticated? }
 
-    include Authentication::ViaMagicLink, LoginHelper
+    include Authentication::ViaMagicLink, UrlHelper
   end
 
   class_methods do
