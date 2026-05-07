6c44153fbadcac7574eab7361078aa038734bb30
yuler <is.yuler@gmail.com>
2026-05-07 18:10:58 +0800

    🔧 [core]: Configure mailer with environment variables

--- core/app/mailers/application_mailer.rb
+++ core/app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: ENV.fetch("MAILER_FROM", "Typo <hi@yuler.cc>")
+
   layout "mailer"
 end
--- core/config/environments/production.rb
+++ core/config/environments/production.rb
@@ -58,16 +58,21 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
+  config.action_mailer.default_url_options = { host: ENV.fetch("SITE_DOMAIN", "example.com") }
+  config.action_mailer.default_options = { from: ENV.fetch("MAILER_FROM", "hi@yuler.cc") }
 
-  # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
-  # config.action_mailer.smtp_settings = {
-  #   user_name: Rails.application.credentials.dig(:smtp, :user_name),
-  #   password: Rails.application.credentials.dig(:smtp, :password),
-  #   address: "smtp.example.com",
-  #   port: 587,
-  #   authentication: :plain
-  # }
+  # Specify outgoing SMTP server.
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              ENV.fetch("SMTP_ADDRESS", "smtp.example.com"),
+    port:                 ENV.fetch("SMTP_PORT", 587).to_i,
+    domain:               ENV.fetch("SMTP_DOMAIN", "example.com"),
+    user_name:            ENV.fetch("SMTP_USERNAME", nil),
+    password:             ENV.fetch("SMTP_PASSWORD", nil),
+    authentication:       "plain",
+    enable_starttls_auto: true,
+    tls:                  ENV.fetch("SMTP_SSL_ENABLED", "false") == "true"
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
