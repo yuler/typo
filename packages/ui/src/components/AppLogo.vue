2f7fd257605e6b6ee1817b30450e1ff89f14e275
yuler <is.yuler@gmail.com>
2026-05-27 18:34:02 +0800

    feat(ui): migrate AppLogo component

---
+++ packages/ui/src/components/AppLogo.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { cn } from '../lib/utils'
+import logoUrl from '../../assets/logo.svg'
+import logoDarkUrl from '../../assets/logo-dark.svg'
+
+interface Props {
+  drag?: boolean
+  version?: string
+  class?: string
+  dark?: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  drag: false,
+  dark: false,
+})
+</script>
+
+<template>
+  <div
+    :class="cn('relative flex flex-col items-center shrink-0 h-8 w-8', props.class)"
+    :data-tauri-drag-region="drag ? true : undefined"
+  >
+    <img v-if="!dark" :src="logoUrl" alt="logo" class="w-full h-full object-contain pointer-events-none">
+    <img v-else :src="logoDarkUrl" alt="logo" class="w-full h-full object-contain pointer-events-none">
+    <span v-if="version" class="text-[8px] absolute -top-1 -right-1 text-muted-foreground pointer-events-none">v{{ version }}</span>
+  </div>
+</template>
